dd10da6ee8db339debd5600631b56009798ad4aa
Saroj Sah <sarojsaroj390@gmail.com>
2025-06-25 10:14:37 +0200

    fixed import statement for Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
-import Header from "./components/Layout/Header";
-import Footer from "./components/Layout/Footer";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import { Toaster } from "./components/ui/toaster";
